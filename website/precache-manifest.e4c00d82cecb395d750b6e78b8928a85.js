35b763e0a8c612f2f2f5039383edf5b5e613adaf
VanneveljTemp2 <jer_vannevel@hotmail.com>
2019-01-03 17:53:05 +0000

    added build files

---
+++ website/precache-manifest.e4c00d82cecb395d750b6e78b8928a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "097b2e60f3ad07cbb50c",
+    "url": "/website/static/js/main.097b2e60.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "097b2e60f3ad07cbb50c",
+    "url": "/website/static/css/main.ed8342e1.chunk.css"
+  },
+  {
+    "revision": "2c18306b458203d423fd9ec0b3102580",
+    "url": "/website/index.html"
+  }
+];
