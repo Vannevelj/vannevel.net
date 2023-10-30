4293dd0749a2c06bd5af3fb69b63f8fd2801f979
Vannevelj <jer_vannevel@outlook.com>
2023-10-30 00:46:19 +0000

    Deploying to master from @ Vannevelj/vannevel.net@5821faf91c7c18a9f06e4a056c7d4a91f6f8373e 🚀

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-8152c1b9f5b7f0ef77b9.js","85ef78b3efecf9b112b9aed354780978"],["/commons-50675cee3a38850a85f7.js","23b203f1a23510ca321ec2101a4241ed"],["/index.html","fc725dca42a945e47b6d7ff7327e7711"],["/offline-plugin-app-shell-fallback/index.html","118e9ba1423d7a6f5bacc61cfc13e471"]];
+var precacheConfig = [["/app-8152c1b9f5b7f0ef77b9.js","85ef78b3efecf9b112b9aed354780978"],["/commons-50675cee3a38850a85f7.js","23b203f1a23510ca321ec2101a4241ed"],["/index.html","74066182dc2280f8379b696267d0f939"],["/offline-plugin-app-shell-fallback/index.html","b8c848aaf1aadd33dcac55a03b3c0645"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
