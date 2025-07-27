5379ae914c7845fb86128cd4441584a6d6aaac1c
Vannevelj <jer_vannevel@outlook.com>
2025-07-27 16:49:18 +0000

    Deploying to gh-pages from @ Vannevelj/vannevel.net@1acbcb67d74576cd01442f4fa6ddbf7c8dea84fe 🚀

--- internal/testing/mocks/author.ts
+++
@@ -1,8 +0,0 @@
-import contacts from "./contacts";
-
-export default {
-  title: "John Doe",
-  photo: "/photo.jpg",
-  description: "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
-  contacts,
-};
